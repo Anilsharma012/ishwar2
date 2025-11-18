2c3b5853c501598714891d19c925c02a5d75ec3f
sachin takoria <sachintakoria2204@gmail.com>
2025-11-18 11:53:13 +0530

    Initial clean commit

---
+++ client/pages/Agricultural.tsx
@@ -0,0 +1,12 @@
+import CategoryPage from "./CategoryPage";
+
+export default function Agricultural() {
+  return (
+    <CategoryPage
+      categoryName="Agricultural"
+      categorySlug="agriculturall"
+      categoryIcon="🌾"
+      categoryDescription="Agricultural land and farming properties in Rohtak"
+    />
+  );
+}
